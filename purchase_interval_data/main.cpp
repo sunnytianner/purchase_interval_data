1ef8eaf44a5b333bbec7ba505e817973f2fac8ee
Eagle_Han <tian19880220@126.com>
2013-07-13 22:47:09 +0800

    Initial Commit

---
+++ purchase_interval_data/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  purchase_interval_data
+//
+//  Created by Eagle on 13-7-13.
+//  Copyright (c) 2013年 Eagle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
